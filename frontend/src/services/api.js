5ad8ea7f076bf1d65d6a22a3a27c00ecbefb9dae
moradiyaom <ommoradiya22@gmail.com>
2026-03-11 14:58:49 +0530

    c
    c

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL}/api` 
+    : '/api',
 });
 
 // Add token to requests
